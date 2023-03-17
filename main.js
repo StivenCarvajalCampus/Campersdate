6a135463e7f5f7df54b6fe70087fb53d2cf89ec9
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-03-17 09:26:13 -0500

    feat(cambios): :sparkles: cambios

--- main.js
+++ main.js
@@ -44,32 +44,53 @@ myFormularioTeams.addEventListener("submit", (e)=>{
     delete data.sede;
     campus[`${sede}`]["Teams"].unshift(data);
     console.log(campus);    
+    listar_teams('[name="sedeTrainers"]','[name="selector_teamtrainers"]')
+    listar_teams('[name="sedeCampers"]','[name="selector_team"]')
+    myFormularioTeams.reset();
     myFormularioTeams.reset();
 })
+
+let listar_teams= (sedeTeams,selector_team) =>{
+    let opc_sede = document.querySelector(sedeTeams);
+    let opc_team= document.querySelector(selector_team);
+    opc_sede.addEventListener('change', (e) => {
+        opc_team.innerHTML = null; 
+        campus[e.target.value]["Teams"].forEach((team) => {
+            opc_team.innerHTML += `
+            <option value = "${team.nombreteam}">${team.nombreteam}</option>
+            `
+            
+        });
+    })
+}
+
+
+
 
 myFormularioCampers.addEventListener("submit", (e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
     console.log(data);
-    let listar_teams= (selector_sede,selector_team) =>{
-        let opc_sede = document.querySelector(selector_sede);
-        let opc_team= document.querySelector(selector_team);
-        opc_sede.addEventListener('change', (e) => {
-            opc_team.innerHTML = null;
-            campus[e.target.value]["Teams"].forEach((team) => {
-                opc_team.innerHTML += `
-                <option value = "${team.nombreteam}">${team.nombreteam}</option>
-                `
-                
-            });
-        })
-    }
+
     let sede = data.sedeCampers;
     delete data.sede;
     campus[`${sede}`]["Camper"].unshift(data);
     console.log(campus);    
     myFormularioCampers.reset();
 })
+let listar_teamstrainer= (sedeTeams,selector_teamtrainer) =>{
+    let opc_sede = document.querySelector(sedeTeams);
+    let opc_team= document.querySelector(selector_teamtrainer);
+    opc_sede.addEventListener('change', (e) => {
+        opc_team.innerHTML = null; 
+        campus[e.target.value]["Teams"].forEach((team) => {
+            opc_team.innerHTML += `
+            <option value = "${team.nombreteam}">${team.nombreteam}</option>
+            `
+            
+        });
+    })
+}
 myFormularioTrainers.addEventListener("submit", (e)=>{
     e.preventDefault();
     let data = Object.fromEntries(new FormData(e.target));
@@ -78,6 +99,7 @@ myFormularioTrainers.addEventListener("submit", (e)=>{
     delete data.sede;
     campus[`${sede}`]["Trainers"].unshift(data);
     console.log(campus);
+
     myFormularioTrainers.reset();
 })
 myFormularioNiveles.addEventListener("submit", (e)=>{
